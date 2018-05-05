45a58891edbcb066e728cf63f393a696825f0a7e
leidy <Ladyale@misena.edu.co>
2018-05-04 19:59:11 -0500

    evaluacion2

---
+++ Evaluacion/resultado.js
@@ -0,0 +1,17 @@
+funtion resultado(){
+	var a=number(document.getElementById('a').value;
+	var b=number(document.getElementById('b').value;
+	var c=number(document.getElementById('c').value;
+	var res1=document.getElementById('res1').value;
+	var res1=document.getElementById('res2').value;
+	
+	coeficiente 1 = parseInt(a);
+    coeficiente 2 = parseInt(b);
+	coeficiente 4 = parseInt(c);
+    resultado = parseInt(resultado);
+    resultado = a * b;
+	
+	resultado =document.getElementById('resultado').value;
+	return false;
+	
+}
